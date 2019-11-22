9788fcd2390c2eb171060ff93e63cca6132340a1
Marco Secchi <loziosecchi@gmail.com>
2019-11-22 10:28:42 +0100

    Add new decisions.

---
+++ TopDownEngineExtensions/Scripts/AI/Graph/Decisions/AIDecisionGroundedNode.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a TopDown Engine <see cref="MoreMountains.TopDownEngine.AIDecisionGrounded"/> decision.
+    /// </summary>
+    [CreateNodeMenu("AI/Decision/Grounded")]
+    public class AIDecisionGroundedNode : AIDecisionNode
+    {
+        public float groundedBufferDelay = 0.2f;
+
+        public override AIDecision AddDecisionComponent(GameObject go)
+        {
+            var decision = go.AddComponent<AIDecisionGrounded>();
+            decision.Label = label;
+            decision.GroundedBufferDelay = groundedBufferDelay;
+            return decision;
+        }
+    }
+}
---
+++ TopDownEngineExtensions/Scripts/AI/Graph/Decisions/Editor/AIDecisionGroundedNodeEditor.cs
@@ -0,0 +1,24 @@
+﻿using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEditor;
+using XNodeEditor;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    [CustomNodeEditor(typeof(AIDecisionGroundedNode))]
+    public class AIDecisionGroundedNodeEditor : AIDecisionNodeEditor
+    {
+        private SerializedProperty _groundedBufferDelay;
+        
+        public override void OnBodyGUI()
+        {
+            base.OnBodyGUI();
+            
+            _groundedBufferDelay = serializedObject.FindProperty("groundedBufferDelay");
+
+            serializedObject.Update();
+            EditorGUIUtility.labelWidth = 120;
+            NodeEditorGUILayout.PropertyField(_groundedBufferDelay);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
