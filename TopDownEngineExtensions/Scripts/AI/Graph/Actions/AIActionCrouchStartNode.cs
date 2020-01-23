661e5865e91fc4470a4191dc9bb122b45b9790ae
marcosecchi <loziosecchi@gmail.com>
2020-01-23 12:27:23 +0100

    Add AIAction Start/Stop Nodes.

---
+++ TopDownEngineExtensions/Scripts/AI/Graph/Actions/AIActionCrouchStartNode.cs
@@ -0,0 +1,21 @@
+﻿using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEngine;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a TopDown Engine <see cref="MoreMountains.TopDownEngine.AIActionCrouchStart"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/Crouch Start")]
+    public class AIActionCrouchStartNode : AIActionNode
+    {
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionCrouchStart>();
+            action.Label = label;
+            return action;
+        }
+    }
+}
