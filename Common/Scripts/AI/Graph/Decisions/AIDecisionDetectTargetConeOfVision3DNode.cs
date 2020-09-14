18f8b1eb371b84b2f08baacf922fd61dc99b996f
marcosecchi <loziosecchi@gmail.com>
2020-09-14 12:22:27 +0200

    Improve DetectTargetConeOfVision3D.

--- Common/Scripts/AI/Graph/Decisions/AIDecisionDetectTargetConeOfVision3DNode.cs
+++ Common/Scripts/AI/Graph/Decisions/AIDecisionDetectTargetConeOfVision3DNode.cs
@@ -11,11 +11,13 @@ namespace TheBitCave.TopDownEngineExensions.AI.Graph
     [CreateNodeMenu("AI/Decision/3D/Detect Target Cone Of Vision")]
     public class AIDecisionDetectTargetConeOfVision3DNode : AIDecisionNode
     {
+        public bool setTargetToNullIfNotFound;
 
         public override AIDecision AddDecisionComponent(GameObject go)
         {
-            var decision = go.AddComponent<AIDecisionDetectTargetConeOfVision2D>();
+            var decision = go.AddComponent<AIDecisionDetectTargetConeOfVision3D>();
             decision.Label = label;
+            decision.SetTargetToNullIfNotFound = setTargetToNullIfNotFound;
             return decision;
         }
     }
