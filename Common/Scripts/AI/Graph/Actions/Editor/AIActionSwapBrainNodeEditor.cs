768c26e45e0ea2f47c927ed9633ce7366739d37a
marcosecchi <loziosecchi@gmail.com>
2020-12-28 19:08:13 +0100

    Add AIActionSwapBrainNode.

---
+++ Common/Scripts/AI/Graph/Actions/Editor/AIActionSwapBrainNodeEditor.cs
@@ -0,0 +1,26 @@
+﻿using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEditor;
+using XNodeEditor;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    [CustomNodeEditor(typeof(AIActionSwapBrainNode))]
+    public class AIActionSwapBrainNodeEditor : AIActionNodeEditor
+    {
+        // private SerializedProperty _newAIBrain;
+
+        protected override void SerializeAdditionalProperties()
+        {
+            // _newAIBrain = serializedObject.FindProperty("newAIBrain");
+
+            serializedObject.Update();
+            // NodeEditorGUILayout.PropertyField(_newAIBrain);
+            serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.Space();
+            EditorGUILayout.HelpBox(
+                "New AI Brain property is hidden. You will have to manually add it once the generator has been executed or assign it at runtime.",
+                MessageType.Warning);
+        }
+
+    }
+}
