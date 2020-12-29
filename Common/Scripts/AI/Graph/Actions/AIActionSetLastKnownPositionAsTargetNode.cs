58348c40b138d0eee8cd5cb4f345573014484c65
marcosecchi <loziosecchi@gmail.com>
2020-12-29 20:37:34 +0100

    Fix broken scenes.

---
+++ Common/Scripts/AI/Graph/Actions/AIActionSetLastKnownPositionAsTargetNode.cs
@@ -0,0 +1,21 @@
+﻿using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEngine;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a TopDown Engine <see cref="MoreMountains.TopDownEngine.AIActionSetLastKnownPositionAsTarget"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/Set Last Known Position as Target")]
+    public class AIActionSetLastKnownPositionAsTargetNode : AIActionNode
+    {
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionSetLastKnownPositionAsTarget>();
+            action.Label = label;
+            return action;
+        }
+    }
+}
