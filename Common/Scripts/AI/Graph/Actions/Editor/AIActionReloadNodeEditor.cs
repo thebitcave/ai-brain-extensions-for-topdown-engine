b42b7254811aaf72bb288a992d7e07427886b19f
marcosecchi <loziosecchi@gmail.com>
2021-06-29 16:08:00 +0200

    Add AIActionReload.

---
+++ Common/Scripts/AI/Graph/Actions/Editor/AIActionReloadNodeEditor.cs
@@ -0,0 +1,23 @@
+﻿using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEditor;
+using UnityEngine;
+using XNodeEditor;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    [CustomNodeEditor(typeof(AIActionReloadNode))]
+    public class AIActionReloadNodeEditor : AIActionNodeEditor
+    {
+        private SerializedProperty _onlyReloadOnceInThisState;
+
+        protected override void SerializeAdditionalProperties()
+        {
+            _onlyReloadOnceInThisState = serializedObject.FindProperty("onlyReloadOnceInThisState");
+
+            serializedObject.Update();
+            EditorGUIUtility.labelWidth = 100;
+            NodeEditorGUILayout.PropertyField(_onlyReloadOnceInThisState);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
