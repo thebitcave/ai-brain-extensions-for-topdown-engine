cb8c5fe7e1ff1225ffc3a464d464e329ddc132a5
marcosecchi <loziosecchi@gmail.com>
2021-06-29 16:34:33 +0200

    Add AIActionMoveAwayFromTarget2D and AIActionMoveAwayFromTarget3D nodes.

---
+++ Common/Scripts/AI/Graph/Actions/AIActionMoveAwayFromTarget3DNode.cs
@@ -0,0 +1,25 @@
+﻿using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEngine;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a TopDown Engine <see cref="MoreMountains.TopDownEngine.AIActionMoveAwayFromTarget3D"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/3D/Move Away From Target")]
+    public class AIActionMoveAwayFromTarget3DNode : AIActionNode
+    {
+        public float maximumDistance = 10.0f;
+
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionMoveAwayFromTarget3D>();
+            action.Label = label;
+            action.MaximumDistance = maximumDistance;
+            
+            return action;
+        }
+    }
+}
---
+++ Common/Scripts/AI/Graph/Actions/Editor/AIActionMoveAwayFromTarget2DNodeEditor.cs
@@ -0,0 +1,23 @@
+﻿using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEditor;
+using UnityEngine;
+using XNodeEditor;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    [CustomNodeEditor(typeof(AIActionMoveAwayFromTarget2DNode))]
+    public class AIActionMoveAwayFromTarget2DNodeEditor : AIActionNodeEditor
+    {
+        private SerializedProperty _maximumDistance;
+
+        protected override void SerializeAdditionalProperties()
+        {
+            _maximumDistance = serializedObject.FindProperty("maximumDistance");
+
+            serializedObject.Update();
+            EditorGUIUtility.labelWidth = 150;
+            NodeEditorGUILayout.PropertyField(_maximumDistance);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
