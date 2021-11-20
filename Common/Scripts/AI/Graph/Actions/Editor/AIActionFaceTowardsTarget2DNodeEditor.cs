4c763248e917163868891f92b5ac54e90df2d525
marcosecchi <loziosecchi@gmail.com>
2021-11-20 16:48:27 +0100

    Add AIActionFaceTowardsTarget2DNode.

---
+++ Common/Scripts/AI/Graph/Actions/Editor/AIActionFaceTowardsTarget2DNodeEditor.cs
@@ -0,0 +1,22 @@
+﻿using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEditor;
+using XNodeEditor;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    [CustomNodeEditor(typeof(AIActionFaceTowardsTarget2DNode))]
+    public class AIActionFaceTowardsTarget2DNodeEditor : AIActionNodeEditor
+    {
+        private SerializedProperty _mode;
+
+        protected override void SerializeAdditionalProperties()
+        {
+            _mode = serializedObject.FindProperty("mode");
+
+            serializedObject.Update();
+            NodeEditorGUILayout.PropertyField(_mode);
+            serializedObject.ApplyModifiedProperties();
+        }
+
+    }
+}
