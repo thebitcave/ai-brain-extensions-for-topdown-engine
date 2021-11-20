b08c3ccbd359b7927dbce41735f2ba11aa4550c3
marcosecchi <loziosecchi@gmail.com>
2021-11-20 16:35:01 +0100

    Add AIActionInvertPatrolDirectionNode.

--- Common/Scripts/AI/Graph/Actions/Editor/AIActionInvertPatrolDirectionNodeEditor.cs
+++ Common/Scripts/AI/Graph/Actions/Editor/AIActionInvertPatrolDirectionNodeEditor.cs
@@ -7,5 +7,23 @@ namespace TheBitCave.TopDownEngineExensions.AI.Graph
     [CustomNodeEditor(typeof(AIActionInvertPatrolDirectionNode))]
     public class AIActionInvertPatrolDirectionNodeEditor : AIActionNodeEditor
     {
+//        private SerializedProperty _movePatrol2D;
+//        private SerializedProperty _movePatrol3D;
+
+        protected override void SerializeAdditionalProperties()
+        {
+//            _movePatrol2D = serializedObject.FindProperty("movePatrol2D");
+//            _movePatrol3D = serializedObject.FindProperty("movePatrol3D");
+
+            serializedObject.Update();
+//            NodeEditorGUILayout.PropertyField(_movePatrol2D);
+//            NodeEditorGUILayout.PropertyField(_movePatrol3D);
+            
+            EditorGUILayout.HelpBox(
+                "Move Patrol 2D and 3D properties are hidden. You will have to manually add them once the generator has been executed or assign them at runtime.",
+                MessageType.Warning);
+
+            serializedObject.ApplyModifiedProperties();
+        }
     }
 }
