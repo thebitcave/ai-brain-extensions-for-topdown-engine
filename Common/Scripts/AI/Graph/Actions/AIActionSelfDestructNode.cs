0b27ecb195fa4a3757a37e6585f53905db0f4e13
marcosecchi <loziosecchi@gmail.com>
2022-01-03 08:57:04 +0100

    Add AIActionSelfDestructNode.

---
+++ Common/Scripts/AI/Graph/Actions/AIActionSelfDestructNode.cs
@@ -0,0 +1,26 @@
+﻿using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEngine;
+
+namespace TheBitCave.TopDownEngineExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a TopDown Engine <see cref="MoreMountains.TopDownEngine.AIActionSelfDestruct"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/Self Destruct")]
+
+    public class AIActionSelfDestructNode : AIActionNode
+    {
+        public bool onlyRunOnce = true;
+        
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionSelfDestruct>();
+            action.Label = label;
+            action.OnlyRunOnce = onlyRunOnce;
+            
+            return action;
+        }
+    }
+}
